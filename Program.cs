3221232e02cd8fcb1d4db3360ca72f023270952b
allan02machado <70041162+allan02machado@users.noreply.github.com>
2020-09-18 18:51:42 -0300

    Este commit adiciona resposta ao exercício

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite algo: ");
+            string texto = Console.ReadLine();  
+            string novoTexto = texto.Replace("r", "l")
+                .Replace("R", "L");
+            Console.WriteLine(novoTexto);
+        }
+    }
+}
